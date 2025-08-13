63183011550e29eba6b993d79f4fadc1c2dcbfc1
Elisandro-dev <150935182+Elisandro-dev@users.noreply.github.com>
2025-08-12 21:51:00 -0300

    upload files

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('formManutencao').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert("Chamado enviado com sucesso!\nPrevisão de atendimento: até 48h.");
+});
