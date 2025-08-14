04f3bb67da47dba1ffae4aae94f6733658f7875a
Elisandro-dev <150935182+Elisandro-dev@users.noreply.github.com>
2025-08-13 21:49:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
 document.getElementById('formManutencao').addEventListener('submit', function(event) {
-  event.preventDefault();
+  event.preventDefault(); // impede o envio padrão para tratar manualmente
+
+  // Mostra a mensagem
   alert("Chamado enviado com sucesso!\nPrevisão de atendimento: até 48h.");
+
+  // Chama a função que prepara e abre o e-mail
+  enviarEmail();
 });
